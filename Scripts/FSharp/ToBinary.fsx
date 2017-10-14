01f730049552aed5b7e18dc025ce2a84dfbbcaa5
wk-j <somnuk.wk@gmail.com>
2017-10-15 00:31:09 +0700

    Refactor

--- Scripts/FSharp/ToBinary.fsx
+++ Scripts/FSharp/ToBinary.fsx
@@ -4,16 +4,16 @@ let  f k =
         match i with
         | 0 | 1 -> [string i ]
         | _ -> (pos (i / 2)) @ pos (i % 2)
-
     if k < 0 then 
-        let rs = pos (-k) |> List.map(fun x -> if x = "0" then "1" else "0")
-        let f a x = 
+        let wise x = if x = "0" then "1" else "0"
+        let plus a x = 
             match x, a with
             | "0", "0" -> "0", "0"
             | "1", "1" -> "0", "1"
             | _  -> "1", "0"
-        List.mapFoldBack f rs "1" |> fst
-
+        let rs = pos (-k) |> List.map(wise)
+        let fst, snd = List.mapFoldBack plus rs "1"
+        if snd = "1" then snd :: fst else fst
     else pos k
 
 f  9 |> printfn "%A"
